a16e8bf46a20feee9e5f024714a8334c7cbd5311
Aviv Eyal <avivey@gmail.com>
2015-01-29 14:28:39 -0800

    a different minify?

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
 rm load-nodejs-modules.c.js ext/nodejs-modules.min.js
 nodejs `which browserify` app/load-nodejs-modules.js --standalone externals -o load-nodejs-modules.c.js
-nodejs `which minify` load-nodejs-modules.c.js -o ext/nodejs-modules.min.js
+nodejs `which minify` load-nodejs-modules.c.js > ext/nodejs-modules.min.js
 rm load-nodejs-modules.c.js
